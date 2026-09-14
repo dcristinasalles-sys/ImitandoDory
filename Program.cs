78496c1af73bef0b6ffd142cf42756dccb1d296c
Daniela <d.cristina.salles@gmail.com>
2026-09-14 20:46:54 -0300

    Correçao

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("---ImitandoDory---\n");
+
+Console.Write("Procurando Nemo:");
+String Frase = Console.ReadLine()!;
+
+String FraseEmbaleies = Frase 
+.Replace("a" , "aaa")
+.Replace("A" , "AAA")
+.Replace("e" , "eee")
+.Replace("E" , "EEE")
+.Replace("i" , "iii")
+.Replace("I" , "III")
+.Replace("o" , "ooo")
+.Replace("O" , "OOO")
+.Replace("u" , "uuu")
+.Replace("U" , "UUU")
+;
+
+Console.WriteLine($"\nEm baleies:\n\n{FraseEmbaleies}");
